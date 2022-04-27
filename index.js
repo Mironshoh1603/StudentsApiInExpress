81060ab8673c7262026970b1bdc327cb79f87b60
mironshoh1603 <mironshohasadov1603.gmail.com>
2022-04-27 19:16:49 +0500

    with morgan and routed

--- index.js
+++ index.js
@@ -1,95 +1,22 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
+
+app.use(morgan("dev"));
+
+app.use((request, response, next) => {
+  request.time = Date.now();
+  next();
+});
 
 let students = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/student.json`, "utf-8")
 );
 
-app.get("/api/v1/student", (request, response) => {
-  response.status(200).json({
-    status: "Ok",
-    data: { students },
-  });
-});
-app.get("/api/v1/student/:id", (req, res) => {
-  const id = +req.params.id;
-  const selectObj = students.find((val) => {
-    return val.id === id;
-  });
-  console.log(selectObj);
-  res.status(selectObj ? 200 : 404).json({
-    status: selectObj ? "OK" : "Fail",
-    data: selectObj ? selectObj : "No data",
-  });
-});
-
-app.post("/api/v1/student", (req, res) => {
-  const data = req.body;
-  // console.log(data);
-  const newId = students[students.length - 1].id + 1;
-  const completeObj = Object.assign({ id: newId }, data);
-  students.push(completeObj);
-  fs.writeFile(
-    `${__dirname}/dev-data/student.json`,
-    JSON.stringify(students),
-    "utf-8",
-    (err) => {
-      res.status(200).json({
-        status: "OK",
-        data: completeObj,
-      });
-    }
-  );
-});
-
-app.delete("/api/v1/student/:id", (req, res) => {
-  const id = +req.params.id;
-  const newObj = students.filter((val) => {
-    return val.id !== id;
-  });
-  const deleteObj = students.find((val) => {
-    return val.id === id;
-  });
-  fs.writeFile(
-    `${__dirname}/dev-data/student.json`,
-    JSON.stringify(newObj),
-    "utf-8",
-    (err) => {
-      res.status(204).json({
-        status: deleteObj ? "OK" : "Failed",
-        data: deleteObj ? "Deleted information" : "Error",
-      });
-    }
-  );
-});
-
-app.put("/api/v1/student/:id", (req, res) => {
-  const newId = +req.params.id;
-  const body = req.body;
-  let selectObj = students.find((val) => {
-    return val.id === newId;
-  });
-  const index = students.indexOf(selectObj);
-  selectObj = Object.assign({ id: newId }, body);
-  students.fill(selectObj, index, index + 1);
-  // console.log(students);
-  fs.writeFile(
-    `${__dirname}/dev-data/student.json`,
-    JSON.stringify(students),
-    "utf-8",
-    (err) => {
-      res.status(200).json({
-        status: "OK",
-        data: selectObj,
-      });
-    }
-  );
-});
-
-app.patch("/api/v1/student/:id/", (req, res) => {
+const UpdateStudentInfo = (req, res) => {
   const newId = +req.params.id;
   const body = req.body;
   let selectObj = students.find((val) => {
@@ -119,6 +46,90 @@ app.patch("/api/v1/student/:id/", (req, res) => {
       });
     }
   );
-});
+};
+const getStudent = (request, response) => {
+  response.status(200).json({
+    status: "Ok",
+    data: { students },
+  });
+};
+const getStudentById = (req, res) => {
+  const id = +req.params.id;
+  const selectObj = students.find((val) => {
+    return val.id === id;
+  });
+  res.status(selectObj ? 200 : 404).json({
+    status: selectObj ? "OK" : "Fail",
+    data: selectObj ? selectObj : "No data",
+  });
+};
+const addStudent = (req, res) => {
+  const data = req.body;
+  // console.log(data);
+  const newId = students[students.length - 1].id + 1;
+  const completeObj = Object.assign({ id: newId }, data);
+  students.push(completeObj);
+  fs.writeFile(
+    `${__dirname}/dev-data/student.json`,
+    JSON.stringify(students),
+    "utf-8",
+    (err) => {
+      res.status(200).json({
+        status: "OK",
+        data: completeObj,
+      });
+    }
+  );
+};
+const deleteStudent = (req, res) => {
+  const id = +req.params.id;
+  const newObj = students.filter((val) => {
+    return val.id !== id;
+  });
+  const deleteObj = students.find((val) => {
+    return val.id === id;
+  });
+  fs.writeFile(
+    `${__dirname}/dev-data/student.json`,
+    JSON.stringify(newObj),
+    "utf-8",
+    (err) => {
+      res.status(204).json({
+        status: deleteObj ? "OK" : "Failed",
+        data: deleteObj ? "Deleted information" : "Error",
+      });
+    }
+  );
+};
+const updateStudent = (req, res) => {
+  const newId = +req.params.id;
+  const body = req.body;
+  let selectObj = students.find((val) => {
+    return val.id === newId;
+  });
+  const index = students.indexOf(selectObj);
+  selectObj = Object.assign({ id: newId }, body);
+  students.fill(selectObj, index, index + 1);
+  // console.log(students);
+  fs.writeFile(
+    `${__dirname}/dev-data/student.json`,
+    JSON.stringify(students),
+    "utf-8",
+    (err) => {
+      res.status(200).json({
+        status: "OK",
+        data: selectObj,
+      });
+    }
+  );
+};
+
+app.route("/api/v1/student").get(getStudent).post(addStudent);
+app
+  .route("/api/v1/student/:id")
+  .get(getStudentById)
+  .patch(UpdateStudentInfo)
+  .put(updateStudent)
+  .delete(deleteStudent);
 
 app.listen(8000, "127.0.0.1");
